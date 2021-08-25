2310509da79f920311f9f7bd108181f06addc923
Max Graman <me@maxg.xyz>
2021-08-25 17:58:14 -0400

    Fixed broken migrations

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,7 @@ const main = async () => {
     type: "postgres",
     url: process.env.DATABASE_URL,
     logging: true,
-    synchronize: __prod__ ? false : true,
+    //synchronize: true,
     migrations: [path.join(__dirname, "./migrations/*")],
     entities: [User],
   });
