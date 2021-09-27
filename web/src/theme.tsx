f1c5257650df32260f0987603d33aa2c1d526736
Max Graman <me@maxg.xyz>
2021-09-27 11:18:33 -0400

    Added color and RAleway

--- web/src/theme.tsx
+++ web/src/theme.tsx
@@ -1,7 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
 import { createBreakpoints } from "@chakra-ui/theme-tools";
-
-const fonts = { mono: `'Menlo', monospace` };
+import "@fontsource/raleway";
 
 const breakpoints = createBreakpoints({
   sm: "40em",
@@ -12,9 +11,65 @@ const breakpoints = createBreakpoints({
 
 const theme = extendTheme({
   colors: {
-    black: "#16161D",
+    black: "#0D0D0D",
+    white: "#F2F2F2",
+    gray: {
+      //Based on #404040
+      50: "#fbf0f2",
+      100: "#dcd8d9",
+      200: "#bfbfbf",
+      300: "#a6a6a6",
+      400: "#8c8c8c",
+      500: "#737373",
+      600: "#595959",
+      700: "#404040",
+      800: "#282626",
+      900: "#150a0d",
+    },
+    blue: {
+      //Based on #80b5f4
+      50: "#e2f2ff",
+      100: "#b7d6fc",
+      200: "#8abbf5",
+      300: "#5d9ff0",
+      400: "#3384ea",
+      500: "#1c6ad1",
+      600: "#1353a3",
+      700: "#0a3b75",
+      800: "#022348",
+      900: "#000d1c",
+    },
+    purple: {
+      //Based on #E6E2F0
+      50: "#f1eef6",
+      100: "#d3cee0",
+      200: "#b6accd",
+      300: "#998abb",
+      400: "#7d6aa9",
+      500: "#63508f",
+      600: "#4d3e6f",
+      700: "#372c4f",
+      800: "#211b2f",
+      900: "#0b0911",
+    },
+    pink: {
+      //Based on #EBD9D9
+      50: "#fceced",
+      100: "#e5cdcd",
+      200: "#d0acae",
+      300: "#bd8b92",
+      400: "#ac6b77",
+      500: "#925162",
+      600: "#723f50",
+      700: "#522d3c",
+      800: "#321a25",
+      900: "#16070f",
+    },
   },
-  fonts,
+  fonts: {
+    heading: "",
+    body: "Raleway",
+  },
   breakpoints,
   icons: {
     logo: {
