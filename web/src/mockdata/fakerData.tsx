d62dd3d777eaaba93154b09d27ec3d5b8ae9d666
torserver <torcatobvaz@gmail.com>
2021-11-03 19:01:37 -0400

    mock data with faker

---
+++ web/src/mockdata/fakerData.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+var faker = require("faker");
+var fs = require('fs');
+
+function generatePets() {
+
+    for (let id = 1; id <= 20; id++) {
+
+        let pets = [];
+
+        // initialize variables with random data
+        var petName = faker.random.firstname;
+        var petType = "dog";
+        var petPrice = faker.random.datatype.number({ min: "25", max: "300" });
+        //var petImage = faker.random.animal.image.cat;
+        var petBio = faker.random.words(15);
+
+        pets.push({
+            "id": id,
+            "pet_name": petName,
+            "pet_type": petType,
+            "pet_price": petPrice,
+            "pet_bio": petBio
+        });
+
+        return {"PetData": pets}
+
+    }
+
+    let dataObj = generatePets();
+
+    fs.writeFileSync('petData.json', JSON.stringify(dataObj, null, '\t'));
+
+}
+
+
