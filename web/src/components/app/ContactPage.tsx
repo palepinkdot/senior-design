3e025b7ada0b8c42e69dcb9d937936b21bb55119
Kyle Marler <kylemarler@outlook.com>
2021-11-21 19:13:16 -0500

    More changes

--- web/src/components/app/ContactPage.tsx
+++ web/src/components/app/ContactPage.tsx
@@ -48,6 +48,8 @@ import {
     MdOutlineEmail,
 } from 'react-icons/md';
 import { BsGithub, BsDiscord, BsPerson } from 'react-icons/bs';
+import {Stack} from "@chakra-ui/core";
+import {FeatureProps} from "framer-motion/dist/framer-motion";
 
 export default function ContactPage() {
     return (
@@ -77,7 +79,7 @@ export default function ContactPage() {
                                                 color="#DCE2FF"
                                                 _hover={{ border: '2px solid #1C6FEB' }}
                                                 leftIcon={<MdPhone color="#1970F1" size="20px" />}>
-                                                +91-988888888
+                                                +1(555) 555-5555
                                             </Button>
                                             <Button
                                                 size="md"
@@ -184,6 +186,88 @@ export default function ContactPage() {
                     </Box>
                 </Box>
             </Flex>
+        </Container>
+    );
+}
+
+
+const Feature = ({ text, icon, iconBg }: FeatureProps) => {
+    return (
+        <Stack direction={'row'} align={'center'}>
+            <Flex
+                w={8}
+                h={8}
+                align={'center'}
+                justify={'center'}
+                rounded={'full'}
+                bg={iconBg}>
+                {icon}
+            </Flex>
+            <Text fontWeight={600}>{text}</Text>
+        </Stack>
+    );
+};
+
+export default function SplitWithImage() {
+    return (
+        <Container maxW={'5xl'} py={12}>
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                <Stack spacing={4}>
+                    <Text
+                        textTransform={'uppercase'}
+                        color={'blue.400'}
+                        fontWeight={600}
+                        fontSize={'sm'}
+                        bg={useColorModeValue('blue.50', 'blue.900')}
+                        p={2}
+                        alignSelf={'flex-start'}
+                        rounded={'md'}>
+                        Our Story
+                    </Text>
+                    <Heading>A digital Product design agency</Heading>
+                    <Text color={'gray.500'} fontSize={'lg'}>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                        nonumy eirmod tempor invidunt ut labore
+                    </Text>
+                    <Stack
+                        spacing={4}
+                        divider={
+                            <StackDivider
+                                borderColor={useColorModeValue('gray.100', 'gray.700')}
+                            />
+                        }>
+                        <Feature
+                            icon={
+                                <Icon as={IoAnalyticsSharp} color={'yellow.500'} w={5} h={5} />
+                            }
+                            iconBg={useColorModeValue('yellow.100', 'yellow.900')}
+                            text={'Business Planning'}
+                        />
+                        <Feature
+                            icon={<Icon as={IoLogoBitcoin} color={'green.500'} w={5} h={5} />}
+                            iconBg={useColorModeValue('green.100', 'green.900')}
+                            text={'Financial Planning'}
+                        />
+                        <Feature
+                            icon={
+                                <Icon as={IoSearchSharp} color={'purple.500'} w={5} h={5} />
+                            }
+                            iconBg={useColorModeValue('purple.100', 'purple.900')}
+                            text={'Market Analysis'}
+                        />
+                    </Stack>
+                </Stack>
+                <Flex>
+                    <Image
+                        rounded={'md'}
+                        alt={'feature image'}
+                        src={
+                            'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                        }
+                        objectFit={'cover'}
+                    />
+                </Flex>
+            </SimpleGrid>
         </Container>
     );
 }
