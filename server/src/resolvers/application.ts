cac1bab84f4d9cfaf8937f25820bf5034dd427b0
Kyle Marler <kylemarler@outlook.com>
2022-02-19 23:05:02 -0500

    Application mutation created

--- server/src/resolvers/application.ts
+++ server/src/resolvers/application.ts
@@ -8,15 +8,8 @@ import {
     Query, InputType,
 } from "type-graphql";
 import { MyContext } from "../types";
-import { Org } from "../entities/Org";
-import argon2 from "argon2";
-import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from "../constants";
-import { sendEmail } from "../utils/sendEmail";
 import { v4 } from "uuid";
 import { getConnection } from "typeorm";
-import { OrgUsernamePasswordInput } from "./OrgUsernamePasswordInput";
-import { validateRegisterOrg } from "../utils/validateRegisterOrg";
-import {CreateAnimalInput} from "./CreateAnimalInput";
 import {Application} from "../entities/Application";
 
 @ObjectType()
@@ -32,18 +25,19 @@ class ApplicationResponse {
     @Field(() => [ApplicationFieldError], { nullable: true })
     errors?: ApplicationFieldError[];
 
-    @Field(() => Org, { nullable: true })
+    @Field(() => Application, { nullable: true })
     application?: Application;
 }
 
 @Resolver()
 export class ApplicationResolver {
     @Query(() => String)
-    helloAnimal() {
+    helloApplication() {
         return "hello world from application";
     }
     @Mutation(() => ApplicationResponse)
-    async createApplication(@Ctx() { req }: MyContext): Promise<ApplicationResponse> {
+    async createApplication(@Arg("animalId") animalId: string,
+                            @Arg("status") status: string, @Ctx() { req }: MyContext): Promise<ApplicationResponse> {
         console.log(req.session.userId);
 
         let application;
@@ -53,9 +47,9 @@ export class ApplicationResolver {
                 .insert()
                 .into(Application)
                 .values({
-                    animalId: Math.random().toString(),
-                    userId: Math.random().toString(),
-                    status: Math.random().toString(),
+                    animalId: animalId,
+                    userId: req.session.userId,
+                    status: status,
                 })
                 .returning("*")
                 .execute();
@@ -76,20 +70,4 @@ export class ApplicationResolver {
         return { application: application };
     }
 
-
-    @Mutation(() => Boolean)
-    logout(@Ctx() { req, res }: MyContext) {
-        return new Promise((resolve) =>
-            req.session.destroy((err) => {
-                res.clearCookie(COOKIE_NAME);
-                if (err) {
-                    console.log(err);
-                    resolve(false);
-                    return;
-                }
-
-                resolve(true);
-            })
-        );
-    }
 }
