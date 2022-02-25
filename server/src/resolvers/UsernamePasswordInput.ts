23c66ae758ce41f07e3f8a8356373685b6a711f5
Max Graman <me@maxg.xyz>
2022-02-25 17:35:09 -0500

    fix for registration

--- server/src/resolvers/UsernamePasswordInput.ts
+++ server/src/resolvers/UsernamePasswordInput.ts
@@ -2,16 +2,20 @@ import { Field, InputType } from "type-graphql";
 
 @InputType()
 export class UsernamePasswordInput {
-  @Field()
-  email: string;
-  @Field()
-  username: string;
-  @Field()
-  password: string;
-  @Field()
-  verifypassword: string;
-  @Field()
-  firstname: string;
-  @Field()
-  lastname: string;
+	@Field()
+	email: string;
+	@Field()
+	username: string;
+	@Field()
+	password: string;
+	@Field()
+	verifypassword: string;
+	@Field()
+	firstname: string;
+	@Field()
+	lastname: string;
+	@Field()
+	phone: string;
+	@Field()
+	zip: string;
 }
