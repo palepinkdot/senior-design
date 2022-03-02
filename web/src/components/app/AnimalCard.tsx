6f95c361ed150d2fb8c0bc2365d1d8a75b2050eb
torserver <torcatobvaz@gmail.com>
2022-03-01 21:14:52 -0500

    fixed multiple animal cards displaying on bigger screen sizes

--- web/src/components/app/AnimalCard.tsx
+++ web/src/components/app/AnimalCard.tsx
@@ -31,84 +31,89 @@ export const AnimalCard: React.FC<AnimalDataProps> = ({ data }) => {
     // background-size: cover;
     // background-position: center;
     return (
-      <Center 
+      // <Flex
+      //   alignItems="center"
+      //   justifyContent="center"
+      // >
+      <Center
+        // height="1200px"
         verticalAlign={"center"}
         justifyContent="center"
-        mb="25rem"
-        >
-      <Box
-        key={data.id}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        flexDirection="column"
-        boxShadow="xl"
-        borderRadius="20px"
-        bgColor="white"        
+        pb="80%"
+        // pt="25%"
+        mb="25%"
       >
-        <HStack mt="80px" display={"flex"}>
-          <VStack alignItems="left" p="6">
-            <Text fontSize="md">{data.type}</Text>
-            <Heading fontSize="5xl" fontWeight="900">
-              {data.name}
-            </Heading>
-            <Text fontSize="lg">4 years old</Text>
-            <Text fontSize="md" as="i">
-              fee: ${data.cost}
-            </Text>
-          </VStack>
-          <Image
-            maxW="550px"
-            w={"70%"}
-            src={data.imageURL}
-            borderRadius="20px"
-            alignItems="center"
-            justifyContent="center"
-            display="flex"
-          >
-          </Image>
-        </HStack>
-        &nbsp;
-        <HStack pt="5">
-        <PetDetailModal pet={data} />
-        &nbsp;
         <Box
-        cursor={'pointer'}
-        as="a"
-        p={4}
-        borderRadius="full"
-        transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
-        bgColor="blue.400"
-        _hover={{
-          bgColor: "red.300",
-          transform: "scale(1.05)",
-        }}
-        _active={{
-          transform: "scale(0.95)",
-        }}
-      >
-        <Text
-          as="i"
-          px="10"
-          color="white"
-          fontSize="1.2rem"
-          fontWeight="bold"
-          textTransform="uppercase"
+          key={data.id}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+          boxShadow="xl"
+          borderRadius="20px"
+          bgColor="white"
         >
-          Adopt Now
-        </Text>
-      </Box>
-      </HStack>
-      &nbsp;
-      </Box>
+          <HStack mt="80px" display={"flex"}>
+            <VStack alignItems="left" p="6">
+              <Text fontSize="md">{data.type}</Text>
+              <Heading fontSize="5xl" fontWeight="900">
+                {data.name}
+              </Heading>
+              <Text fontSize="lg">4 years old</Text>
+              <Text fontSize="md" as="i">
+                fee: ${data.cost}
+              </Text>
+            </VStack>
+            <Image
+              maxW="550px"
+              w={"70%"}
+              src={data.imageURL}
+              borderRadius="20px"
+              alignItems="center"
+              justifyContent="center"
+              display="flex"
+            >
+
+            </Image>
+          </HStack>
+          &nbsp;
+          <HStack pt="5">
+            <PetDetailModal pet={data} />
+            &nbsp;
+            <Box
+              cursor={"pointer"}
+              as="a"
+              p={4}
+              borderRadius="full"
+              transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+              bgColor="blue.400"
+              _hover={{
+                bgColor: "red.300",
+                transform: "scale(1.05)",
+              }}
+              _active={{
+                transform: "scale(0.95)",
+              }}
+            >
+              <Text
+                as="i"
+                px="10"
+                color="white"
+                fontSize="1.2rem"
+                fontWeight="bold"
+                textTransform="uppercase"
+              >
+                Adopt Now
+              </Text>
+            </Box>
+          </HStack>
+          &nbsp;
+        </Box>
       </Center>
+      // </Flex>
     );
   }
 };
-
-
-
-
 
 
 //
