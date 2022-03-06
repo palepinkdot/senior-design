09129ed3cad1b2ec58a06731a68153fe9e1e6100
torserver <torcatobvaz@gmail.com>
2022-03-06 16:38:42 -0500

    create sendEmail function that accepts agency email

--- server/src/utils/sendGrid.ts
+++ server/src/utils/sendGrid.ts
@@ -1,26 +1,31 @@
-const sgMail = require('@sendgrid/mail')
+export function sendAdoptionRequest (agencyEmail: string) {
 
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const sgMail = require('@sendgrid/mail')
 
-    console.log("Sendgrid API Key retrieved");
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
+  console.log("Sendgrid API Key retrieved");
 
-    const acceptedMSG_ADO = {
-        to: "torcatobvaz@gmail.com", //replace with adopter user email
-        from: "swipet.dev@gmail.com",
-        subject: "Your Pet Adoption Application was Accepted",
-        text: "Congratulations! Please pick up your new pet at the Shelter",
-        html: "<strong>You have helped find a new home, thank you! </strong>",
-      };
+  const acceptedMSG_ADO = {
+      to: agencyEmail,            //replace with adopter user email
+      from: "swipet.dev@gmail.com",
+      subject: "Your Pet Adoption Application was Accepted",
+      text: "Congratulations! Please pick up your new pet at the Shelter",
+      html: "<strong>You have helped find a new home, thank you! </strong>",
+    };
 
-      console.log(sgMail);
+    console.log(sgMail);
 
-      sgMail
-      .send(acceptedMSG_ADO)
-      .then(() => {}, (error: { response: { body: any; }; }) => {
-        console.error(error);
-    
-        if (error.response) {
-          console.error(error.response.body)
-        }
-      });
+    sgMail
+    .send(acceptedMSG_ADO)
+    .then(() => {}, (error: { response: { body: any; }; }) => {
+      console.error(error);
+  
+      if (error.response) {
+        console.error(error.response.body)
+      }
+    });
+
+    return null;
+}
+
